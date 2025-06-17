378629942954ee87c73287c7212ff75eafdcaca1
Konadean <93809485+Konadean@users.noreply.github.com>
2025-06-17 15:43:15 -0400

    added more items!
    +power pill
    +fabric softener
    +cursed nail

--- itemBank.py
+++ itemBank.py
@@ -5,7 +5,10 @@ class ItemBank:
         self.items = [
             "Health Potion",
             "Stamina Potion",
-            "Bomb"
+            "Bomb",
+            "Power Pill",
+            "Fabric Softener",
+            "Cursed Nail"
         ]
 
     def use_item(self, user, target, item):
@@ -16,6 +19,12 @@ class ItemBank:
                 self._stamina_potion(user)
             case "Bomb":
                 self._bomb(user, target)
+            case "Power Pill":
+                self._power_pill(user)
+            case "Fabric Softener":
+                self._fabric_softener(user, target)
+            case "Cursed Nail":
+                self._cursed_nail(user, target)
 
     def _health_potion(self, user):
         print(user.name + "'s HP has been fully restored!")
@@ -24,12 +33,13 @@ class ItemBank:
     def _stamina_potion(self, user):
         print(user.name + "'s SP has been fully restored!")
         user.sp = user.max_sp
+
     def _bomb(self, user, target):
         print(user.name + " threw a bomb at " + target.name + "!")
         print('...\n')
         time.sleep(1.5)
         boom = random.random()
-        if 0 <= boom and boom <= 0.75:
+        if 0 <= boom and boom <= 0.70:
             print("     _.-^^---....,,--      ")
             print(" _--                  --_  ")
             print("<                        >)")
@@ -45,3 +55,31 @@ class ItemBank:
             target.hp = 0
         else:
             print("Fuse didn't light... Nothing happened.")
+
+    def _power_pill(self, user):
+        print(user.name + "'s ATK has been permanently increased by 5!!!")
+        user.atk += 5
+
+    def _fabric_softener(self,user,target):
+        print(target.name+ "'s DEF has been reduced by 5!")
+        user.atk = user.atk - 5 if user.atk - 5 > 0 else 0
+    
+    def _cursed_nail(self,user,target):
+        print("""
+     .-.
+   .'   `.
+   :g g   :
+   : o    `.
+  :         ``.
+ :             `.
+:  :         .   `.
+:   :          ` . `.
+ `.. :            `. ``;
+    `:;             `:'
+       :              `.
+        `.              `.     .
+          `'`'`'`---..,___`;.-'
+            """)
+        print("OOoooOOOooOOooo!! A ghost drains both " + user.name + "'s and " + target.name + "'s life to 1 HP!")
+        user.hp = 1
+        target.hp = 1
