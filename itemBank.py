bbacff264dc56a4bbd445ff947ca489641abd2ce
Konadean <93809485+Konadean@users.noreply.github.com>
2025-06-17 10:11:20 -0400

    Added Item Functionality

--- itemBank.py
+++ itemBank.py
@@ -1,8 +1,52 @@
+import time
+import random
 class ItemBank:
     def __init__(self):
         self.items = [
             "Health Potion",
             "Stamina Potion",
+            "Bomb"
         ]
 
-    
+    def use_item(self, user, target, item):
+        match item:
+            case "Health Potion":
+                self._health_potion(user)
+            case "Stamina Potion":
+                self._stamina_potion(user)
+            case "Bomb":
+                self._bomb(user, target)
+
+    def _health_potion(self, user):
+        print(user.name + "'s HP has been fully restored!")
+        user.hp = user.max_hp
+
+    def _stamina_potion(self, user):
+        print(user.name + "'s SP has been fully restored!")
+        user.sp = user.max_sp
+
+    def _bomb(self, user, target):
+        print(user.name + " threw a bomb at " + target.name + "!")
+        time.sleep(1)
+        print('.',end='')
+        time.sleep(1)
+        print('.',end='')
+        time.sleep(1)
+        print('.\n')
+        boom = random.random()
+        if 0 <= boom and boom <= 0.65:
+            print("     _.-^^---....,,--      ")
+            print(" _--                  --_  ")
+            print("<                        >)")
+            print("|                         |")
+            print(" \\._                   _./ ")
+            print("    ```--. . , ; .--'''    ")
+            print("          | |   |          ")
+            print("       .-=||  | |=-.       ")
+            print("       `-=#$%&%$#=-'       ")
+            print("          | ;  :|          ")
+            print(" _____.,-#%&$@%#&#~,._____ ")
+            print("\nKABOOM!! " + target.name + " is completely annihilated!\n")
+            target.hp = 0
+        else:
+            print("Fuse didn't light... Nothing happened.")
